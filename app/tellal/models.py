2a66b4e73394fcebf460797090e1d93a66bbe412
mehtapgundogan <mehtapgundogan@gmail.com>
2015-06-15 00:02:25 +0300

    adding language support with a keyError

--- app/tellal/models.py
+++ app/tellal/models.py
@@ -5,9 +5,26 @@ from flask import Flask
 from app import db
 from account.models import User
 
-class Issue(db.Model):
+import sqlalchemy_utils
+from flask.ext.babel import get_locale
+
+
+sqlalchemy_utils.i18n.get_locale = get_locale
+
+from sqlalchemy_i18n import (
+    make_translatable,
+    translation_base,
+    Translatable,
+)
+
+
+make_translatable(options={'locales': ['tr', 'en']})
+
+class Issue(Translatable,db.Model):
 	__tablename__ = "issue"
 	__table_args__ = {"useexisting" : True}
+	__translatable__ = {'locales': ['tr', 'en']}
+    
 	id= db.Column(db.Integer,primary_key=True)
 	owner = db.Column(db.Integer,db.ForeignKey("user.id"))
 	project = db.Column(db.String(256))
